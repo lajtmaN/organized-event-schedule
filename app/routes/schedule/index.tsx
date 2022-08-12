ffd123f77fadb5385df897bf6acec34ae32c078b
Tim Hjermitslev Keren Gjøderum <6584419+lajtmaN@users.noreply.github.com>
2022-08-12 10:36:53 +0200

    Shortcut to schedule for latest event

--- app/routes/schedule/index.tsx
+++ app/routes/schedule/index.tsx
@@ -1,7 +1,22 @@
 import { redirect } from "@remix-run/server-runtime";
+import { prisma } from "~/db.server";
+import { notFound } from "~/server/utils/notFound";
 
-export function loader() {
+export async function loader() {
   // Retrieve the latest event slug and redirect to it.
-  const slug = "latest";
-  return redirect(`/schedule/${slug}`);
+  const latestEvent = await prisma.event.findFirst({
+    where: {
+      published: true,
+    },
+    orderBy: {
+      startDate: "desc",
+    },
+    select: {
+      slug: true,
+    },
+  });
+  if (!latestEvent) {
+    return notFound("No published events found");
+  }
+  return redirect(`/schedule/${latestEvent.slug}`);
 }
